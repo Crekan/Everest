28c3d3e382fc11f9c5d37dd886d829a4c399aabe
Egor-Babashko <egor.babashko2005@mail.ru>
2022-11-09 14:12:13 +0300

    home + catalog pages

---
+++ front/src/js/pages/CatalogPage.vue
@@ -0,0 +1,64 @@
+<template>
+  <main-layout-component>
+    <main class="container">
+      <div class="catalog">
+        <h1 class="sc-title sc-title_y">{{ catalog[catalog.length - 1].title }}</h1>
+        <section class="catalog__content">
+          <catalog-item-component
+              v-for="item in catalog[0]"
+              :item="item"
+              :key="item.id"
+          />
+        </section>
+      </div>
+    </main>
+  </main-layout-component>
+</template>
+
+<script>
+import { useRouter, useRoute } from 'vue-router'
+
+import {
+  // ref,
+  // reactive,
+  // toRefs,
+  // isRef,
+  // isReactive,
+  // computed,
+  // watch
+  // onMounted,
+  // onUpdated,
+  // computed,
+} from 'vue'
+
+import MainLayoutComponent from "@/js/layouts/MainLayoutComponent";
+// import axios from "axios";
+import { useStore } from "vuex";
+import CatalogItemComponent from "@/js/components/CatalogPageComponents/CatalogItemComponent";
+
+export default {
+  name: "CatalogPage",
+  setup() {
+    const store = useStore()
+    const router = useRouter()
+    const route = useRoute()
+
+    const catalog = store.getters["Catalogs/getCatalogItems"][route.params.id]
+
+    return {
+      catalog,
+
+      router,
+      route,
+    }
+  },
+  components: {
+    CatalogItemComponent,
+    MainLayoutComponent
+  },
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ front/src/js/store/modules/catalogs.js
@@ -0,0 +1,209 @@
+export default {
+    namespaced: true,
+    state: {
+        catalogItems: [
+            [
+                [
+                    {id: 0, name: "Алкидные эмали", img: '/assets/img/CatalogPage/Enamels/alkyd-enamels.svg'},
+                    {id: 1, name: "Акриловые эмали", img: '/assets/img/CatalogPage/Enamels/acrylic-enamels.svg'},
+                    {id: 2, name: 'Нитроэмали', img: '/assets/img/CatalogPage/Enamels/nitro-enamels.svg'},
+                    {id: 3, name: "Масляные краски", img: '/assets/img/CatalogPage/Enamels/oil-paints.svg'},
+                    {id: 4, name: "Грунты ГФ и ГЭ", img: '/assets/img/CatalogPage/Enamels/hammer-paints.svg'},
+                    {id: 5, name: "Эмали спецназначения", img: '/assets/img/CatalogPage/Enamels/special-enamels.svg'},
+                    {id: 6, name: "Растворители", img: '/assets/img/CatalogPage/Enamels/solvents.svg'},
+                    {
+                        id: 7,
+                        name: "Специальные грунты",
+                        img: '/assets/img/CatalogPage/Enamels/primers-and-primer-enamels.svg'
+                    },
+                ],
+                {
+                    "alt": "img1",
+                    "id": 0,
+                    "img": "/assets/img/HomePage/catalog-img1.png",
+                    "title": "Эмали"
+                }
+            ],
+            [
+                [
+                    {id: 0, name: "Водно-дисперсионные краски", img: '/assets/img/CatalogPage/Enamels/alkyd-enamels.svg'},
+                    {id: 1, name: "Колеровочные пасты", img: '/assets/img/CatalogPage/Enamels/acrylic-enamels.svg'},
+                    {id: 2, name: 'Аэрозольные краски', img: '/assets/img/CatalogPage/Enamels/nitro-enamels.svg'},
+                    {id: 3, name: "Декоративные штукатурки", img: '/assets/img/CatalogPage/Enamels/oil-paints.svg'},
+                    {id: 4, name: "Колоранты", img: '/assets/img/CatalogPage/Enamels/hammer-paints.svg'},
+                    {id: 5, name: "Шпатлевки готовые", img: '/assets/img/CatalogPage/Enamels/special-enamels.svg'},
+                    {id: 6, name: "Акриловые грунты", img: '/assets/img/CatalogPage/Enamels/solvents.svg'},
+                    {
+                        id: 7,
+                        name: "Специальные грунты",
+                        img: '/assets/img/CatalogPage/Enamels/primers-and-primer-enamels.svg'
+                    },
+                ],
+                {
+                    "alt": "img2",
+                    "id": 1,
+                    "img": "/assets/img/HomePage/catalog-img2.png",
+                    "title": "Краски"
+                }
+            ],
+            [
+                [
+                    {id: 0, name: "Для дерева", img: '/assets/img/CatalogPage/Enamels/alkyd-enamels.svg'},
+                    {id: 1, name: "Огне-биозащита, антисептики", img: '/assets/img/CatalogPage/Enamels/acrylic-enamels.svg'},
+                    {id: 2, name: 'Для сауны', img: '/assets/img/CatalogPage/Enamels/nitro-enamels.svg'},
+                    {id: 3, name: "Лак ХВ", img: '/assets/img/CatalogPage/Enamels/oil-paints.svg'},
+                    {id: 4, name: "Морилки", img: '/assets/img/CatalogPage/Enamels/hammer-paints.svg'},
+                    {id: 5, name: "Нитролаки", img: '/assets/img/CatalogPage/Enamels/special-enamels.svg'},
+                    {id: 6, name: "Пропитки", img: '/assets/img/CatalogPage/Enamels/solvents.svg'},
+                    {id: 7, name: "Прочее", img: '/assets/img/CatalogPage/Enamels/solvents.svg'},
+                    {
+                        id: 7,
+                        name: "Специальные грунты",
+                        img: '/assets/img/CatalogPage/Enamels/primers-and-primer-enamels.svg'
+                    },
+                ],
+                {
+                    "alt": "img3",
+                    "id": 2,
+                    "img": "/assets/img/HomePage/catalog-img3.png",
+                    "title": "Защита для древесины"
+                }
+            ],
+            [
+                [
+                    {id: 0, name: "Пены монтажные", img: '/assets/img/CatalogPage/Enamels/alkyd-enamels.svg'},
+                    {id: 1, name: "Герметики", img: '/assets/img/CatalogPage/Enamels/acrylic-enamels.svg'},
+                    {id: 2, name: 'Мастики', img: '/assets/img/CatalogPage/Enamels/nitro-enamels.svg'},
+                    {id: 3, name: "Жидкие гвозди", img: '/assets/img/CatalogPage/Enamels/oil-paints.svg'},
+                    {id: 4, name: "ПВА", img: '/assets/img/CatalogPage/Enamels/hammer-paints.svg'},
+                    {id: 5, name: "Контактный клей", img: '/assets/img/CatalogPage/Enamels/special-enamels.svg'},
+                    {id: 6, name: "Клей для обоев", img: '/assets/img/CatalogPage/Enamels/solvents.svg'},
+                    {id: 7, name: "Для напольных покрытий", img: '/assets/img/CatalogPage/Enamels/acrylic-enamels.svg'},
+                    {id: 8, name: 'Для потолочных и декоративных плит', img: '/assets/img/CatalogPage/Enamels/nitro-enamels.svg'},
+                    {id: 9, name: "Универсальные клея", img: '/assets/img/CatalogPage/Enamels/oil-paints.svg'},
+                    {id: 10, name: "Холодная сварка", img: '/assets/img/CatalogPage/Enamels/hammer-paints.svg'},
+                    {id: 11, name: "Эпоксидные клея", img: '/assets/img/CatalogPage/Enamels/special-enamels.svg'},
+                    {
+                        id: 7,
+                        name: "Специальные грунты",
+                        img: '/assets/img/CatalogPage/Enamels/primers-and-primer-enamels.svg'
+                    },
+                ],
+                {
+                    "alt": "img4",
+                    "id": 3,
+                    "img": "/assets/img/HomePage/catalog-img4.png",
+                    "title": "Пены, Герметики, Клей"
+                }
+            ],
+            [
+                [
+                    {id: 0, name: "Измерительне", img: '/assets/img/CatalogPage/Enamels/alkyd-enamels.svg'},
+                    {id: 1, name: "Ленты", img: '/assets/img/CatalogPage/Enamels/acrylic-enamels.svg'},
+                    {id: 2, name: 'Малярный', img: '/assets/img/CatalogPage/Enamels/nitro-enamels.svg'},
+                    {id: 3, name: "Монтажно-крепежный", img: '/assets/img/CatalogPage/Enamels/oil-paints.svg'},
+                    {id: 4, name: "Пистолеты", img: '/assets/img/CatalogPage/Enamels/hammer-paints.svg'},
+                    {id: 5, name: "Абразивные", img: '/assets/img/CatalogPage/Enamels/special-enamels.svg'},
+                    {id: 6, name: "Режущие", img: '/assets/img/CatalogPage/Enamels/solvents.svg'},
+                    {id: 7, name: "Средства защиты труда", img: '/assets/img/CatalogPage/Enamels/oil-paints.svg'},
+                    {id: 8, name: "Прочее", img: '/assets/img/CatalogPage/Enamels/hammer-paints.svg'},
+                    {id: 9, name: "Столярно-слесарные", img: '/assets/img/CatalogPage/Enamels/special-enamels.svg'},
+                    {id: 10, name: "Штукатурные", img: '/assets/img/CatalogPage/Enamels/solvents.svg'},
+                    {
+                        id: 7,
+                        name: "Специальные грунты",
+                        img: '/assets/img/CatalogPage/Enamels/primers-and-primer-enamels.svg'
+                    },
+                ],
+                {
+                    "alt": "img5",
+                    "id": 4,
+                    "img": "/assets/img/HomePage/catalog-img5.png",
+                    "title": "Инструменты"
+                }
+            ],
+            [
+                [
+                    { id: 0, name: "Космофен", img: '/assets/img/CatalogPage/Enamels/alkyd-enamels.svg' },
+                    { id: 1, name: "Откос оконный утепленный", img: '/assets/img/CatalogPage/Enamels/acrylic-enamels.svg' },
+                    { id: 2, name: 'Москитная группа', img: '/assets/img/CatalogPage/Enamels/nitro-enamels.svg' },
+                    { id: 3, name: "Панели ПВХ", img: '/assets/img/CatalogPage/Enamels/oil-paints.svg' },
+                    { id: 4, name: "Подкладки под стеклопакет", img: '/assets/img/CatalogPage/Enamels/hammer-paints.svg' },
+                    { id: 5, name: "Прочее", img: '/assets/img/CatalogPage/Enamels/special-enamels.svg' },
+                    { id: 6, name: "Подоконник", img: '/assets/img/CatalogPage/Enamels/solvents.svg' },
+                    { id: 7, name: "Профили, нащельники", img: '/assets/img/CatalogPage/Enamels/primers-and-primer-enamels.svg' },
+                    { id: 8, name: "Псулы, ленты", img: '/assets/img/CatalogPage/Enamels/primers-and-primer-enamels.svg' },
+                    { id: 9, name: "Ручки", img: '/assets/img/CatalogPage/Enamels/primers-and-primer-enamels.svg' },
+                    { id: 10, name: "Сэндвич-ПВХ", img: '/assets/img/CatalogPage/Enamels/primers-and-primer-enamels.svg' },
+                    { id: 11, name: "Уплотнители оконные", img: '/assets/img/CatalogPage/Enamels/primers-and-primer-enamels.svg' },
+                ],
+                {
+                    "alt": "img6",
+                    "id": 5,
+                    "img": "/assets/img/HomePage/catalog-img6.png",
+                    "title": "Оконная комплектация"
+                }
+            ],
+            [
+                [
+                    { id: 0, name: "Двери входные", img: '/assets/img/CatalogPage/Enamels/alkyd-enamels.svg' },
+                    { id: 1, name: "Двери межкомнатные", img: '/assets/img/CatalogPage/Enamels/acrylic-enamels.svg' },
+                    { id: 2, name: 'Плинтуса, порожки,стремянки', img: '/assets/img/CatalogPage/Enamels/nitro-enamels.svg' },
+                    { id: 3, name: "Сухие смеси", img: '/assets/img/CatalogPage/Enamels/oil-paints.svg' },
+                    { id: 4, name: "Арматура", img: '/assets/img/CatalogPage/Enamels/hammer-paints.svg' },
+                    { id: 5, name: "Сантехника", img: '/assets/img/CatalogPage/Enamels/special-enamels.svg' },
+                    { id: 6, name: "Теплоизоляция", img: '/assets/img/CatalogPage/Enamels/solvents.svg' },
+                    { id: 7, name: "Плинтуса потолочные, угол, плитка, фартук", img: '/assets/img/CatalogPage/Enamels/primers-and-primer-enamels.svg' },
+                    { id: 8, name: "Профили для гипсокартона, комплектующие", img: '/assets/img/CatalogPage/Enamels/primers-and-primer-enamels.svg' },
+                    { id: 9, name: "Электрика", img: '/assets/img/CatalogPage/Enamels/primers-and-primer-enamels.svg' },
+                ],
+                {
+                    "alt": "img6",
+                    "id": 6,
+                    "img": "/assets/img/HomePage/catalog-img7.png",
+                    "title": "Общестроительные материалы"
+                }
+            ],
+            [
+                [
+                    { id: 0, name: "Анкерные болты", img: '/assets/img/CatalogPage/Enamels/alkyd-enamels.svg' },
+                    { id: 1, name: "Гайки", img: '/assets/img/CatalogPage/Enamels/acrylic-enamels.svg' },
+                    { id: 2, name: 'Гвозди', img: '/assets/img/CatalogPage/Enamels/nitro-enamels.svg' },
+                    { id: 3, name: "Дюбельная техника", img: '/assets/img/CatalogPage/Enamels/oil-paints.svg' },
+                    { id: 4, name: "Саморезы и шурупы", img: '/assets/img/CatalogPage/Enamels/hammer-paints.svg' },
+                    { id: 5, name: "Крепеж перфорированный", img: '/assets/img/CatalogPage/Enamels/special-enamels.svg' },
+                    { id: 6, name: "Прочее", img: '/assets/img/CatalogPage/Enamels/solvents.svg' },
+                    { id: 7, name: "Скобы строительные", img: '/assets/img/CatalogPage/Enamels/primers-and-primer-enamels.svg' },
+                    { id: 8, name: "Шпильки резьбовые", img: '/assets/img/CatalogPage/Enamels/primers-and-primer-enamels.svg' },
+                ],
+                {
+                    "alt": "img8",
+                    "id": 7,
+                    "img": "/assets/img/HomePage/catalog-img8.png",
+                    "title": "Крепеж"
+                }
+            ],
+            [
+                [
+                    { id: 0, name: "Батарейки", img: '/assets/img/CatalogPage/Enamels/alkyd-enamels.svg' },
+                    { id: 1, name: "Бытовая химия", img: '/assets/img/CatalogPage/Enamels/acrylic-enamels.svg' },
+                    { id: 2, name: 'Прочие хозтовары', img: '/assets/img/CatalogPage/Enamels/nitro-enamels.svg' },
+                    { id: 3, name: "Лопаты снеговые", img: '/assets/img/CatalogPage/Enamels/oil-paints.svg' },
+                ],
+                {
+                    "alt": "img9",
+                    "id": 8,
+                    "img": "/assets/img/HomePage/catalog-img9.png",
+                    "title": "Хозтовары"
+                }
+            ]
+        ],
+    },
+    getters: {
+        getCatalogItems(state) {
+            return state.catalogItems
+        },
+    },
+    mutations: {},
+    actions: {},
+}
