853265b2199faf0de5fd4f95c5f59dc5694b942a
Vania Kazeko <vania.kazekj.00@bk.ry>
2022-11-09 18:21:58 +0300

    fix product

--- back/everest/product/models.py
+++ back/everest/product/models.py
@@ -2,10 +2,10 @@ from django.db import models
 
 
 class Product(models.Model):  # Albom
-    images = models.ImageField(upload_to='product_images/', verbose_name='Изображение')
+    images = models.ImageField(upload_to='product_images/', verbose_name='Изображение', blank=True, null=True)
     title = models.CharField(max_length=255, verbose_name='Заголовок')
     new_price = models.DecimalField(max_digits=5, decimal_places=2, verbose_name='Новая цена')
-    old_price = models.DecimalField(max_digits=5, decimal_places=2, verbose_name='Старая цена', blank=True)
+    old_price = models.DecimalField(max_digits=5, decimal_places=2, verbose_name='Старая цена', blank=True, null=True)
 
     def __str__(self):
         return self.title
